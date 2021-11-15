558735d845551c06d1bea10c9a0d084f3cd3665c
Antonioangy <dumitruangy26@gmail.com>
2021-11-15 16:51:17 +0100

    html contatti migliorati

--- js/script.js
+++ js/script.js
@@ -6,24 +6,24 @@ var carousel = new Vue ({
             {
                 userImg: 'img/01.jpg',
                 userName: 'Michele',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                text: 'Hai portato a spasso il cane?',
                 status:'sent'
             },
             {
                 userImg: 'img/02.jpg',
                 userName: 'Fabio',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                text: 'Ciao come stai?',
                 status:'sent'
             },
             {
                 userImg: 'img/03.jpg',
                 userName: 'Samuele',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                text: 'La Marianna va in campagna.',
                 status:'received'            },
             {
                 userImg: 'img/04.jpg',
                 userName: 'Luigi',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                text: 'Lo sai che ha aperto una nuova pizzeria?',
                 status:'received'
             }
            
